99ba6436d449b8b5be6c1fef19d08978e700f164
Evan <evfernberg@gmail.com>
2021-07-02 12:18:59 -0400

    Added component for image to be rendered

---
+++ src/pages/image.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { StaticImage } from "gatsby-plugin-image"
+import Pic from '../components/pic'
+
+export default function Image() {
+  return (
+    <div>
+      <Pic />
+    </div>
+  )
+};
