7ba6576948330b4d6f19dc9d03ed64bdd7e2030b
Evan <evfernberg@gmail.com>
2021-07-02 12:54:08 -0400

    Added flying elvis patriots logo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const About = () => {
   return(
@@ -11,6 +12,10 @@ const About = () => {
             About Section
           </h1>
           <p>This is the about page.</p>
+          <StaticImage
+            alt="Flying Elvis"
+            src="../images/flyingElvis.png"
+          />
         </Layout>
     </main>
   )
